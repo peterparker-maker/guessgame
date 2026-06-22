48edbe2b5c2f1bd9212b2dbf61dac699c3e9678f
Peter Kuo <pt861022@gmail.com>
2026-06-22 15:10:52 +0800

    Fix: Use absolute path and conditional base config for production

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: process.env.NODE_ENV === "production" ? "/guessgame/" : "/",
   build: {
     outDir: "dist",
     emptyOutDir: true,
+    minify: true,
   },
 });
