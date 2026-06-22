4e263eb6581d85c70ca7c1f2a0432c89635eaa98
Peter Kuo <pt861022@gmail.com>
2026-06-22 16:55:02 +0800

    Improve: Add explicit rollup config for better asset bundling

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,13 @@ export default defineConfig({
   base: "/guessgame/",
   build: {
     outDir: "docs",
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name]-[hash].js",
+        chunkFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash][extname]",
+      },
+    },
   },
 });
